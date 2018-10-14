d48885442dea627129ee1c27afd7334ae5ed2be6
jkxruby <jkxruby@gmail.com>
2018-10-14 17:05:46 +0800

    start to study java string and so on

---
+++ src/cn/jk/stringbuilder/Test01.java
@@ -0,0 +1,10 @@
+package cn.jk.stringbuilder;
+
+/**
+ * Created by jkx on 2018/10/14.
+ */
+public class Test01 {    // 正常情况下用 StringBulider即可， StringBuffer很少用到
+    StringBuilder sb = new StringBuilder();  // 字符数组长度初始值为 16
+    StringBuilder sb1 = new StringBuilder(32);
+
+}
